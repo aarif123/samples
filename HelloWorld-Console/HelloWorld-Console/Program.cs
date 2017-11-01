61e4f146b76ab7cb64fb0cff902c52de06698c20
Mohammad Aarif <mohammad.aarif@trantorinc.com>
2017-11-01 14:29:34 +0530

    Added sample unit tests

--- HelloWorld-Console/HelloWorld-Console/Program.cs
+++ HelloWorld-Console/HelloWorld-Console/Program.cs
@@ -6,14 +6,22 @@ using System.Threading.Tasks;
 
 namespace HelloWorld_Console
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Enter you name: ");
             string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name + "!");
+            Console.WriteLine(GetNameWithHello(name));
             Console.ReadLine();
+        }
+
+        public static string GetNameWithHello(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException("Not a valid name.");
+
+            return "Hello " + name + "!";
         }
     }
 }
---
+++ HelloWorld-Console/Tests/HelloWorldTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorld_Console;
+
+namespace Tests
+{
+    [TestClass]
+    public class HelloWorldTests
+    {
+        [TestMethod]
+        public void ShouldReturnNameWith_Hello_OnValidNameInput()
+        {
+            string expectedResult = "Hello Mohammad!";
+            string actualResult = Program.GetNameWithHello("Mohammad");
+
+            Assert.AreEqual(expected: expectedResult, actual: actualResult);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThrowNullReferenceExceptionOn_EmptyOrNullNameInput()
+        {
+            Program.GetNameWithHello("");
+        }
+    }
+}
